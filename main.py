a00e43878015988acd38b67a252c1fde8cf66886
Thiago Macedo <tm09529@gmail.com>
2024-07-16 19:18:03 -0300

    fim das circunstancias
    resolvi o bug de importação

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ from routes import users_bp
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from models import db
+from models.user import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Inicializar extensões com a aplicação
 db.init_app(app)
-
-app.register_blueprint(users_bp)
-
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+@login_manager.user_loader
+def load_user(user_id): 
+    return User.query.get(int(user_id))
+
+# Registrar blueprints
+app.register_blueprint(users_bp)
+
+# Criar o banco de dados e tabelas
 with app.app_context():
     db.create_all()
 
