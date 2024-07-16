f2578c1356436cac4f0b2ddb5a8b51eb19ed0fec
Ana Karolina Morais <anakarolinamorais@MacBook-Air-de-Ana.local>
2024-07-15 23:44:34 -0300

    create task and return tasks

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from flask import Flask
+<<<<<<< Updated upstream
+=======
+from config import Config
+from models import db  # Importa db do models
+from routes import tasks_bp
+>>>>>>> Stashed changes
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-app.run(debug=True)
+db.init_app(app)  # Inicializa o db com o app
+
+app.register_blueprint(tasks_bp)
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(debug=True)
